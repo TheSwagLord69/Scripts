f9a0a280d5b139880f0c4cb6f06699a4571bd036
Hentai Salesman 69 <hentaisalesman69@protonmail.com>
2022-04-17 03:42:58 +0800

    Update telegram_export_contact_list_to_vcf.py

--- telegram_export_contact_list_to_vcf.py
+++ telegram_export_contact_list_to_vcf.py
@@ -8,36 +8,40 @@ import sys
 import os
 import re
 
-contact_list = []
-contact_counter = 0
+def main():
+    contact_list = []
+    contact_counter = 0
 
-with open(os.path.abspath(sys.argv[1])) as f:
-    lines = f.readlines()
-    
-'''enumerate lines'''
-for c, i in enumerate(lines):
-    
-    '''seach name'''
-    regex = '<div class="name bold">'
-    if re.search(regex, i):
-        contact_name = str(lines[c+1])
-        contact_number = str(lines[c+5]).replace(" ", "")
+    with open(os.path.abspath(sys.argv[1])) as f:
+        lines = f.readlines()
         
-        '''strip front "+" and all whitespace'''
-        if re.search("[a-z]", contact_number):
-            pass
-        else:
-            contact_list.append({"name":contact_name,"number":contact_number})
+    '''enumerate lines'''
+    for c, i in enumerate(lines):
+        
+        '''seach name'''
+        regex = '<div class="name bold">'
+        if re.search(regex, i):
+            contact_name = str(lines[c+1])
+            contact_number = str(lines[c+5]).replace(" ", "")
             
-'''write to a new contacts.vcf'''
-f = open("newcontacts.vcf", "w")
+            '''strip front "+" and all whitespace'''
+            if re.search("[a-z]", contact_number):
+                pass
+            else:
+                contact_list.append({"name":contact_name,"number":contact_number})
+                
+    '''write to a new contacts.vcf'''
+    f = open("newcontacts.vcf", "w")
 
-for i in contact_list:
-    f.write("BEGIN:VCARD\n")
-    f.write("VERSION:2.1\n")
-    f.write("N:;" + str(i['name']).strip() + ";;;\n")
-    f.write("FN:"+ str(i['name']).strip() +"\n")
-    f.write("TEL;CELL;PREF:"+ str(i['number']).strip() +"\n")
-    f.write("END:VCARD\n")
-    
-f.close()
+    for i in contact_list:
+        f.write("BEGIN:VCARD\n")
+        f.write("VERSION:2.1\n")
+        f.write("N:;" + str(i['name']).strip() + ";;;\n")
+        f.write("FN:"+ str(i['name']).strip() +"\n")
+        f.write("TEL;CELL;PREF:"+ str(i['number']).strip() +"\n")
+        f.write("END:VCARD\n")
+        
+    f.close()
+
+if __name__ == "__main__":
+    main()
